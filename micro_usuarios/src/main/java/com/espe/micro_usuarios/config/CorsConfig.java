f58445c37c86c2c248b7a71ddffc7920891ce0ae
Nohely Scarleth Villegas Menendez <107574681+NohelyVillegas@users.noreply.github.com>
2025-01-23 22:36:02 -0500

    Add files via upload

---
+++ micro_usuarios/src/main/java/com/espe/micro_usuarios/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.espe.micro_usuarios.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite todas las rutas
+                .allowedOrigins("http://localhost:3000") // Permite solicitudes desde tu frontend en React
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                .allowedHeaders("*") // Permite todos los headers
+                .allowCredentials(true); // Permite credenciales (si usas cookies o tokens)
+    }
+}
